18aeb286e3f624a98de42a9aba803a5238997bbe
23jy0130 <23jy0130@jec.ac.jp>
2024-10-16 15:29:03 +0900

    Create HelloWorld.java
    First Create

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HellowWorld {
+  public static void main(String[] args){
+    System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+  }
+}
