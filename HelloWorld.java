0dc988d88d8848af96b4478c6b2e77b4662f5f7a
`ピァンパット` <`23jy0130@jec.ac.jp`>
2024-10-23 16:08:08 +0900

    日本電子専門学校を複数表示

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,10 @@ public class HellowWorld {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
         System.out.println("日本電子専門学校");
+        System.out.println("日本電子専門学校");
+        System.out.println("日本電子専門学校");
+        System.out.println("日本電子専門学校");
+        System.out.println("日本電子専門学校");
+        System.out.println("日本電子専門学校");
   }
 }
